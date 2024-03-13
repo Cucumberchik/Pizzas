a29d72157d051fcc2b30d5801cb26127e666b9fd
Kukumberchick <durovalexei93@gmail.com>
2024-03-13 11:55:17 +0600

    fixed bug

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import App from './App.tsx'
 import './index.scss'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
-import { store } from './redux/store.tsx'git 
+import { store } from './redux/store.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
